f7ebb107329d1d4debfee61e18efc65b52c575d9
dzhindzhi <vippersonal1331@gmail.com>
2022-12-21 10:48:22 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import App from "./App";
+import ReactDOM from 'react-dom/client';
+import React from "react";
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+   <App></App>
+  </>
+);
